4c70ee4d2e853177e5d782616c8ae74998175fe1
Roy <128028057+loinguyen-2311@users.noreply.github.com>
2025-04-02 13:08:57 +0700

    Delete COPY ./entrypoint.sh  directory

--- COPY ./entrypoint.sh /entrypoint.sh
+++
@@ -1,9 +0,0 @@
-FROM oryd/kratos:latest
-
-# Copy configuration files
-COPY ./kratos.yml /etc/config/kratos.yml
-COPY ./identity.schema.json /etc/config/identity.schema.json
-COPY ./entrypoint.sh /entrypoint.sh
-
-# Set the entrypoint
-ENTRYPOINT ["/entrypoint.sh"]
